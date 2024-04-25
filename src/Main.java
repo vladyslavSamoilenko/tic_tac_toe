cdd1343ff7fd2709dcd45288975eb2505a201111
Vladyslav Samoilenko <dushasam26@gmail.com>
2024-04-25 19:00:18 +0200

    Created printGameTable function

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,29 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Use the following mapping table to specify a cell using numbers from 1 to 9:");
         printTableMapping();
+        char [][] gameTable = {{' ', ' ', ' '},{' ', ' ', ' '},{' ', ' ', ' '}};
         if (new Random().nextBoolean()) {
-            makeComputerMowe();
-            printGameTable();
+            makeComputerMowe(gameTable);
+            printGameTable(gameTable);
         }
         while (true) {
-            makeUserMowe();
-            printGameTable();
-            if (isUserWin()) {
+            makeUserMowe(gameTable);
+            printGameTable(gameTable);
+            if (isUserWin(gameTable)) {
                 System.out.println("YOU WIN");
                 break;
             }
-            if (isDraw()) {
+            if (isDraw(gameTable)) {
                 System.out.println("SORRY DRAW");
                 break;
             }
-            makeComputerMowe();
-            printGameTable();
-            if (isComputerWon()) {
+            makeComputerMowe(gameTable);
+            printGameTable(gameTable);
+            if (isComputerWon(gameTable)) {
                 System.out.println("COMPUTER WIN");
                 break;
             }
-            if (isDraw()) {
+            if (isDraw(gameTable)) {
                 System.out.println("SORRY DRAW");
                 break;
             }
@@ -34,7 +35,7 @@ public class Main {
         System.out.println("GAME OVER");
     }
 
-    private static boolean isComputerWon() {
+    private static boolean isComputerWon(char [][] gameTable) {
         return false;
     }
 
@@ -49,21 +50,29 @@ public class Main {
 
     }
 
-    private static void makeComputerMowe() {
+    private static void makeComputerMowe(char [][] gameTable) {
+
     }
 
-    private static boolean isDraw() {
+    private static boolean isDraw(char [][] gameTable) {
         return false;
     }
 
-    private static boolean isUserWin() {
+    private static boolean isUserWin(char [][] gameTable) {
         return false;
     }
 
-    private static void printGameTable() {
+    private static void printGameTable(char [][] gameTable) {
+        System.out.println( "-------------");
+        System.out.println("|" + " " + gameTable[2][0]  + " " +"|"+ " " +gameTable[2][1]+ " " +"|"+ " " +gameTable[2][2]+ " " +"|");
+        System.out.println( "-------------");
+        System.out.println("|" + " " + gameTable[1][0]  + " " +"|"+ " " +gameTable[1][1]+ " " +"|"+ " " +gameTable[1][2]+ " " +"|");
+        System.out.println( "-------------");
+        System.out.println("|" + " " + gameTable[0][0]  + " " +"|"+ " " +gameTable[0][1]+ " " +"|"+ " " +gameTable[0][2]+ " " +"|");
+        System.out.println( "-------------");
     }
 
-    private static void makeUserMowe() {
+    private static void makeUserMowe(char [][] gameTable) {
     }
 
 
